2d2e9f1e6622b4da8881e3d05dbbee697c6b71f0
ylan <pepsiwyl@gmail.com>
2024-03-08 10:49:08 +0800

    1.commons-lang3引入

--- src/main/java/org/ylan/common/convention/enums/UserErrorCodeEnum.java
+++ src/main/java/org/ylan/common/convention/enums/UserErrorCodeEnum.java
@@ -9,7 +9,8 @@ import org.ylan.common.convention.errorcode.IErrorCode;
 public enum UserErrorCodeEnum implements IErrorCode {
 
     USER_LOGIN_ERROR("A000201","用户登录失败,用户名或密码错误"),
-    USER_REPEAT_LOGIN_ERROR("A000202","用户重复登录错误");
+    USER_REPEAT_LOGIN_ERROR("A000202","用户重复登录错误"),
+    USER_LOGIN_PARAM_ERROR("A000203","用户登录参数错误");
 
     private final String code;
 
